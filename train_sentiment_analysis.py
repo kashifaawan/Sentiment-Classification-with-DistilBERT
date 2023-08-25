77215b18b1f5ac585ee57f9141a48f0b6a66dbe4
Muhammad Kashif <engr.kashifaawan@gmail.com>
2023-08-25 12:58:42 +0500

    Update train_sentiment_analysis.py

--- train_sentiment_analysis.py
+++ train_sentiment_analysis.py
@@ -92,52 +92,54 @@ class SentimentClassification:
                 total_train_loss += loss.item()
                 correct_train_predictions += (logits.argmax(dim=1) == labels).sum().item()
             
-        # Calculate average training loss and accuracy for the epoch
-        average_train_loss = total_train_loss / len(self.train_loader.dataset)
-        train_accuracy = correct_train_predictions / len(self.train_loader.dataset)
-        
-    # Validation loop
-        model.eval()
-        val_predictions = []
-        val_labels = []
-        total_val_loss = 0.0
-        correct_val_predictions = 0
-        
-        with torch.no_grad():
-            for batch in self.valid_loader:
-                input_ids, labels = batch
-                input_ids, labels = input_ids.to(self.device), labels.to(self.device)
+            # Calculate average training loss and accuracy for the epoch
+            average_train_loss = total_train_loss / len(self.train_loader.dataset)
+            train_accuracy = correct_train_predictions / len(self.train_loader.dataset)
+            
+        # Validation loop
+            model.eval()
+            val_predictions = []
+            val_labels = []
+            total_val_loss = 0.0
+            correct_val_predictions = 0
+            
+            with torch.no_grad():
+                for batch in self.valid_loader:
+                    input_ids, labels = batch
+                    input_ids, labels = input_ids.to(self.device), labels.to(self.device)
 
-                outputs = model(input_ids, labels=labels)
-                logits = outputs.logits
+                    outputs = model(input_ids, labels=labels)
+                    logits = outputs.logits
 
-                total_val_loss += criterion(logits, labels).item()
-                correct_val_predictions += (logits.argmax(dim=1) == labels).sum().item()
-                
-                val_predictions.extend(logits.argmax(dim=1).cpu().numpy())
-                val_labels.extend(labels.cpu().numpy())
-        
-        # Calculate average validation loss and accuracy for the epoch
-        average_val_loss = total_val_loss / len(self.valid_loader.dataset)
-        val_accuracy = correct_val_predictions / len(self.valid_loader.dataset)
+                    total_val_loss += criterion(logits, labels).item()
+                    correct_val_predictions += (logits.argmax(dim=1) == labels).sum().item()
+                    
+                    val_predictions.extend(logits.argmax(dim=1).cpu().numpy())
+                    val_labels.extend(labels.cpu().numpy())
+            
+            # Calculate average validation loss and accuracy for the epoch
+            average_val_loss = total_val_loss / len(self.valid_loader.dataset)
+            val_accuracy = correct_val_predictions / len(self.valid_loader.dataset)
 
-        train_loss_history.append(average_train_loss)
-        valid_loss_history.append(average_val_loss)
-        train_accuracy_history.append(train_accuracy)
-        valid_accuracy_history.append(val_accuracy)
+            train_loss_history.append(average_train_loss)
+            valid_loss_history.append(average_val_loss)
+            train_accuracy_history.append(train_accuracy)
+            valid_accuracy_history.append(val_accuracy)
 
-        print(f"Epoch {epoch + 1}/{self.epochs}")
-        print(f"Training Loss: {average_train_loss:.4f} | Training Accuracy: {train_accuracy * 100:.2f}%")
-        print(f"Validation Loss: {average_val_loss:.4f} | Validation Accuracy: {val_accuracy * 100:.2f}%")
-        
-        if val_accuracy > best_val_accuracy:
-            best_val_accuracy = val_accuracy
-            best_epoch = epoch
-            print(f"Validation accuracy improved to {best_val_accuracy * 100:.2f}% at epoch {best_epoch + 1}.")
-            self.model.save_pretrained(self.output_dir)
-            self.tokenizer.save_pretrained(self.output_dir)
-            print("Model saved.")
-
+            print(f"Epoch {epoch + 1}/{self.epochs}")
+            print(f"Training Loss: {average_train_loss:.4f} | Training Accuracy: {train_accuracy * 100:.2f}%")
+            print(f"Validation Loss: {average_val_loss:.4f} | Validation Accuracy: {val_accuracy * 100:.2f}%")
+            
+            if val_accuracy > best_val_accuracy:
+                best_val_accuracy = val_accuracy
+                best_epoch = epoch
+                if best_epoch > 1:
+                   print(f"Validation accuracy improved to {best_val_accuracy * 100:.2f}% at epoch {best_epoch + 1}.")
+                   print("Model saved.")
+                self.model.save_pretrained(self.output_dir)
+                self.tokenizer.save_pretrained(self.output_dir)
+               
+        print('--------Training process completed---------')
         return train_loss_history, valid_loss_history, train_accuracy_history, valid_accuracy_history
 
     def plot_curves(self, train_loss, valid_loss, train_accuracy, valid_accuracy):
@@ -159,7 +161,7 @@ class SentimentClassification:
         plt.ylabel('Accuracy')
         plt.title('Training and Validation Accuracy')
         plt.tight_layout()
-        plt.savefig('training_stats.png')  # Save the plot to a file
+        plt.savefig('training_validation_curves.png')  # Save the plot to a file
         plt.show()
     def run_training(self):
         data = self.load_data()
@@ -172,14 +174,14 @@ class SentimentClassification:
         self.criterion = torch.nn.CrossEntropyLoss()
 
         train_loss, valid_loss, train_accuracy, valid_accuracy = self.train_model()
-
+        
         self.plot_curves(train_loss, valid_loss, train_accuracy, valid_accuracy)
-
+         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a DistilBERT sentiment analysis model")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
-    parser.add_argument("--epochs", type=int, default=3, help="Number of training epochs")
-    parser.add_argument("--train_file", type=str, default="/content/drive/MyDrive/train.xlsx", help="Path to the training data file")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("--train_file", type=str, default=r"D:\Sentiment-Classification-with-DistilBERT-main\IMDB-Movie-Reviews-Large-Dataset-50k-master\train.xlsx", help="Path to the training data file")
     parser.add_argument("--output_dir", type=str, default="distillbert_sentiment_model", help="Output directory to save the trained model")
 
     args = parser.parse_args()
